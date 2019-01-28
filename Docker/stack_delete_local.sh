e2cb019e26b3babb747ecf0d5b446695fb18aa6b
praveenit219 <praveen.tirunamali@gmail.com>
2019-01-28 14:51:25 +0800

    added docker support with docker swarm script

---
+++ Docker/stack_delete_local.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Destroys the storefront Docker stack
+
+# usage: sh ./stack_delete_local.sh
+
+set -e
+
+docker stack rm digiservices
+
+echo "Destroying the stack...pausing for 30 seconds..."
+sleep 30
+
+docker ps
+
+echo "Script completed..."
---
+++ Docker/stack_deploy_local.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Deploys the storefront Docker stack
+
+# usage: sh ./stack_deploy_local.sh
+
+set -e
+
+# docker swarm init
+
+docker stack deploy -c docker-compose.yml digiservices
+echo "Starting the stack: middleware...pausing for 30 seconds..."
+sleep 30
+
+#docker stack deploy -c docker-compose-services.yml digiservices
+#echo "Starting the stack: services...pausing for 10 seconds..."
+#sleep 10
+
+docker stack ls
+docker stack services digiservices
+docker container ls
+
+echo "Script completed..."
+echo "Services may take up to several minutes to start, fully..."
