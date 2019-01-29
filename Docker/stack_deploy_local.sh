1ef9cdc52804bacbb4307dd8ac74895f0e90af0d
praveenit219 <praveen.tirunamali@gmail.com>
2019-01-29 10:23:37 +0800

    updating docker compose files

--- Docker/stack_deploy_local.sh
+++ Docker/stack_deploy_local.sh
@@ -9,12 +9,12 @@ set -e
 # docker swarm init
 
 docker stack deploy -c docker-compose.yml digiservices
-echo "Starting the stack: middleware...pausing for 30 seconds..."
+echo "Starting the stack: middleware...pausing for 40 seconds..."
 sleep 30
 
-#docker stack deploy -c docker-compose-services.yml digiservices
-#echo "Starting the stack: services...pausing for 10 seconds..."
-#sleep 10
+docker stack deploy -c docker-compose-services.yml digiservices
+echo "Starting the stack: services...pausing for 40 seconds..."
+sleep 10
 
 docker stack ls
 docker stack services digiservices
